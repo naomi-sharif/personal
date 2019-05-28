76d83647796eee931f7d4cc009c14acb16b06cc8
Naomi <naomi@goldspecdigital.com>
2019-05-28 22:21:03 +0100

    Added method to toggle hamburger animation

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,15 @@ const app = {
 
   init() {
     //
-  }, 
+  },
+
+  toggleHamburger() {
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.addEventListener("click", function() {
+      console.log('hello');
+      hamburger.classList.toggle("active");
+    })
+  }
 }
 
 app.init()
